d9758db2142371010ded391613f9df06066aa0e4
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-03-31 11:30:37 +0530

    Update RPi-BreadBoard.py

--- RPi-BreadBoard.py
+++ RPi-BreadBoard.py
@@ -9,8 +9,12 @@ GPIO.setmode(GPIO.BCM)
 #Configure GPIO Pin 17 as output
 GPIO.setup(17,GPIO.OUT) 
 #Set GPIO Pin to High
-GPIO.output(17,GPIO.HIGH)
-#Wait for 1sec
-time.sleep(1)
-#Set GPIO Pin to Low
-GPIO.output(17,GPIO.LOW)
+a=0
+while a<=50:
+  GPIO.output(17,GPIO.HIGH)
+  #Wait for 1sec
+  time.sleep(1)
+  #Set GPIO Pin to Low
+  GPIO.output(17,GPIO.LOW)
+  time.sleep(1)
+  a=a+1
