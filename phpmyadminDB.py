8903a953c9fe6084553e872695cf2ab99360fbaf
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-04-03 10:26:10 +0530

    Update phpmyadminDB.py

--- phpmyadminDB.py
+++ phpmyadminDB.py
@@ -7,9 +7,9 @@ conn =pymysql.connect(database="IoTDataBase",user="krishna",password="KRISHNA",h
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
-data={'ID':22006,'topic':10,'data':45}
+data={'topic':10,'data':45}
 #Execute the SQL to write data to the database
-cur.execute("INSERT INTO <tablename>(name, age, place)VALUES(%(name)s,%(age)s,%(place)s);",data)
+cur.execute("INSERT INTO mqttdata(topic, data)VALUES(%(topic)s,%(data)s);",data)
 #Close the cursor
 cur.close()
 #Commit the data to the database
