5696892f81ceb33664062fc2c7d9b61a46a17a82
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-04-10 10:06:16 +0530

    Create mqtt-pub.py

---
+++ mqtt-pub.py
@@ -0,0 +1,10 @@
+
+
+import paho.mqtt.client as mqtt
+
+payload="Hello:
+topic="IOT/test"
+client = mqtt.Client()
+client.connect('0.0.0.0',1883,60)
+(rc,mid)=client.publish(topic,payload);
+client.disconnect();
