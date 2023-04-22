2dbb5cbba19200e06e5290b50663b6bdb44577de
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-04-22 15:59:28 +0530

    Update mqtt-sub.py

--- mqtt-sub.py
+++ mqtt-sub.py
@@ -5,12 +5,30 @@ global broker;
 global port;
 
 
-broker = "34.131.170.190";
+broker = "34.131.164.58";
 port = 1883;
 
 client_uniq = "pubclient_123"
 
 mqttclient = paho.Client(client_uniq, True) 
+
+def storeData(mesg):
+  #Create a connection to MySQL Database 
+  conn =pymysql.connect(database="senorData",user="user",password="pass123",host="localhost")
+
+  #Create a MySQL Cursor to that executes the SQLs
+  cur=conn.cursor()
+  #Create a dictonary containing the fields, name, age and place
+  data={'topic':'IOT/test','data':mesg}
+  #Execute the SQL to write data to the database
+  cur.execute("INSERT INTO MQTTdata(topic , data)VALUES(%(topic)s,%(data)s);",data)
+  print("Data added")
+  #Close the cursor
+  cur.close()
+  #Commit the data to the database
+  conn.commit()
+  #Close the connection to the database
+  conn.close()
 
 def test(client, userdata, message):
   print("client:"+ str(client))
