dd52e24d340b466466b088adef297abc06442471
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-04-22 16:10:46 +0530

    Update mqtt-sub.py

--- mqtt-sub.py
+++ mqtt-sub.py
@@ -14,8 +14,7 @@ mqttclient = paho.Client(client_uniq, True)
 
 def storeData(mesg):
   #Create a connection to MySQL Database 
-  conn =pymysql.connect(database="senorData",user="user",password="pass123",host="localhost")
-
+  conn =pymysql.connect(database="IoTDataBase",user="krishna",password="KRISHNA",host="localhost")
   #Create a MySQL Cursor to that executes the SQLs
   cur=conn.cursor()
   #Create a dictonary containing the fields, name, age and place
