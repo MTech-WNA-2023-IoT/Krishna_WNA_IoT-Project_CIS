5dbc2616cc8a7f3c830c481253aa5a038345c5dd
krishnamurtichaturvedi <73469163+krishnamurtichaturvedi@users.noreply.github.com>
2023-06-20 19:35:36 +0530

    Create greeshma_MySQL.py

---
+++ greeshma_MySQL.py
@@ -0,0 +1,17 @@
+#Python Program to input data to mysql database
+#Import pymysql module library
+import pymysql
+#Create a connection to MySQL Database 
+conn =pymysql.connect(database="greeshma",user="user",password="PASS",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, switch , its power data
+data={'topic':'t','Sensorvalue':50}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO sensordata(topic, Sensorvalue)VALUES(%(name)s,%(int);",data)
+#Close the cursor
+cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
